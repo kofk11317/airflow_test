1f7835bd4998707d0fda736b149e79642523d444
kofk <kofk113@naver.com>
2025-03-05 15:45:46 +0900

    retry

---
+++ dags/dag_task_decorator.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+import pendulum
+from airflow.decorators import task
+with DAG(
+    dag_id='dag_task_decorator',
+    schedule='@daily',
+    start_date=pendulum.datetime(2025, 1, 1, tz='Asia/Seoul'),
+    catchup=False, # 누락된 구간을 실행하지 않음
+
+ ) as dag:
+    @task(task_id='task1')
+    def print_context(text):
+        print(text)
+    #task id = 실행함수    
+    task1=print_context('decorator')
+    
